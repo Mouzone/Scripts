c0883f22062e113e06c6a2606b49ed2a8728b03a
Mouzone <sunnyliu010@gmail.com>
2025-05-13 12:49:35 -0400

    Add script to start bible verse plugin from Repos directly

---
+++ start-bible-verse-plugin.fish
@@ -0,0 +1,33 @@
+#!/usr/local/bin/fish
+
+# Start hot reload
+osascript -e '
+tell application "iTerm"
+    tell current window
+        create tab with default profile
+        tell current session to write text "cd ~/Repos/obsidian-plugins/.obsidian/plugins/hot-reload && npm run dev"
+    end tell
+end tell'
+
+# Start plugin
+osascript -e '
+tell application "iTerm"
+    tell current window
+        create tab with default profile
+        tell current session to write text "cd ~/Repos/obsidian-plugins/.obsidian/plugins/bible-verse-obsidian && npm run dev"
+    end tell
+end tell'
+
+# Open frontend in Chrome
+open -a "Google Chrome" https://chatgpt.com/
+open -a "Obsidian" obsidian://open?vault=obsidian-plugins
+cd ~/Repos/obsidian-plugins/.obsidian/plugins/bible-verse-obsidian
+code .
+clear
+
+osascript -e '
+tell application "iTerm"
+    tell current window
+        select tab 1
+    end tell
+end tell'
